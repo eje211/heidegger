af10af1cce0b138d1bb94be6123131a986032126
Emmanuel Eytan <list@regularoddity.com>
2013-04-05 10:46:25 -0700

    Renamed static file and added .bat file for windows, as well as basic server script.

--- static/js/chart.js
+++ static/js/chart.js
@@ -14,19 +14,10 @@ $(document).ready(function() {
 
 		canvas.clearRect(0, 0, 250, 250);
 		canvas.beginPath();
-		// canvas.moveTo(115, 115);
-		// canvas.lineTo(115, 135);
-		// canvas.lineTo(135, 135);
-		// canvas.lineTo(135, 115);
-		console.log(e + ', ' + a + ', ' + s + ', ' + k);
 		canvas.moveTo(c - e, c - e);
-		console.log(c - e);
 		canvas.lineTo(c - a, c + a);
-		console.log((c - a) + ', ' + (c + a));
 		canvas.lineTo(c + k, c + k);
-		console.log(c + k);
 		canvas.lineTo(c + s, c - s);
-		console.log((c + s) + ', ' + (c - s));
 		canvas.closePath();
 		canvas.fill();
 		canvas.stroke();
---
+++ web_server.bat
@@ -0,0 +1,1 @@
+C:\Python33\python.exe C:\Users\eeytan\Documents\GitHub\heidegger\web_server.py
