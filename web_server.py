eeee689af892423b6ce36d4b400f48747a80ddac
Emmanuel Eytan <list@regularoddity.com>
2013-04-05 11:40:50 -0700

    Separated static and templates after all...

--- web_server.py
+++ web_server.py
@@ -1,14 +1,41 @@
 import tornado.ioloop
 import tornado.web
+import os
+
+# Global variables
+# Yes, we need some!
+CHARTS     = []
+CONTROLERS = []
+# We'll use the same folder for statics and templates.
+static     = 'static'
+cwd        = os.getcwd()
+knows_exts = ('.png', '.jpg', '.gif', '.ico')
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+    def get(self, page_name):
+        page_name = file_resolver(page_name)
+        if not os.path.exists(page_name):
+            page_name = file_resolver('chart')
+        try: self.write(file_get_contents(page_name))
+        except FileNotFoundError: pass
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+# Global functions
 
-if __name__ == "__main__":
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+def file_resolver(filename):
+    if not filename.endswith(knows_exts):
+	    filename += '.html'
+    filename = '%s/%s' % (static, filename)
+
+def main():
+    application = tornado.web.Application([
+        (r"/(.*)", MainHandler),
+    ])
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
