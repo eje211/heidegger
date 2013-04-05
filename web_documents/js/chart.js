d79e99a7d75ce87faf1bbb30c9f387b397bd328c
Emmanuel <info@regularoddity.com>
2013-04-04 22:51:40 -0700

    It works! At last!

--- web_documents/js/chart.js
+++ web_documents/js/chart.js
@@ -7,22 +7,26 @@ $(document).ready(function() {
 	canvas.lineWidth = 2;
 
 	$('#chart_submit').click(function() {
-		var e = $('#explorer'  ).val(),
-			a = $('#achiever'  ).val(),
-			s = $('#socializer').val(),
-			k = $('#killer'    ).val();
+		var e = parseInt($('#explorer'  ).val()),
+			a = parseInt($('#achiever'  ).val()),
+			s = parseInt($('#socializer').val()),
+			k = parseInt($('#killer'    ).val());
 
 		canvas.clearRect(0, 0, 250, 250);
 		canvas.beginPath();
-		canvas.moveTo(115, 115);
-		canvas.lineTo(115, 135);
-		canvas.lineTo(135, 135);
-		canvas.lineTo(135, 115);
+		// canvas.moveTo(115, 115);
+		// canvas.lineTo(115, 135);
+		// canvas.lineTo(135, 135);
+		// canvas.lineTo(135, 115);
 		console.log(e + ', ' + a + ', ' + s + ', ' + k);
-		// canvas.moveTo(c - e, c - e);
-		// canvas.lineTo(e - a, e + a);
-		// canvas.lineTo(a + k, -a + k);
-		// canvas.lineTo(-k + s, -k - s);
+		canvas.moveTo(c - e, c - e);
+		console.log(c - e);
+		canvas.lineTo(c - a, c + a);
+		console.log((c - a) + ', ' + (c + a));
+		canvas.lineTo(c + k, c + k);
+		console.log(c + k);
+		canvas.lineTo(c + s, c - s);
+		console.log((c + s) + ', ' + (c - s));
 		canvas.closePath();
 		canvas.fill();
 		canvas.stroke();
