7c545fa528beb072d1b28b0a8f61e61e18982704
Emmanuel Eytan <list@regularoddity.com>
2013-04-05 17:49:25 -0700

    Added touch punch.

--- static/js/controller.js
+++ static/js/controller.js
@@ -10,13 +10,23 @@ $(document).ready(function() {
 	$('.slider').each(function() {
 		sliders.push($(this).slider({min: 0, max: 100, value: start_value}, {slide: function(event, ui) {
 			$(this).parents('tr').find('input').val(ui.value);
-			var message = {};
-			$(sliders).each(function() {
-				message[$(this).attr('id').charAt(0)] =
-					parseInt($(this).slider('value'));
-			});
-			ws.send(JSON.stringify(message));
+			send_to_chart();
 		}}))
 	});
+
+	$('input').change(function() {
+		console.log('changed!');
+		$(this).parents('tr').find('.slider').slider('value', $(this).val());
+		send_to_chart();
+	});
+
+	function send_to_chart() {
+		var message = {};
+		$(sliders).each(function() {
+			message[$(this).attr('id').charAt(0)] =
+				parseInt($(this).slider('value'));
+		});
+		ws.send(JSON.stringify(message));
+	}
 
 });
